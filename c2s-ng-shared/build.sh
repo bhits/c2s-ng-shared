51af945bf176e6470fea0400fb8289cf73ec800f
burcakulug <burcak.ulug@feisystems.com>
2017-10-17 16:22:44 -0400

    Add font colors to shell scripts
    #16415:[Beach ball]: As a patient, I can select multiple providers for sharing consent, so that I can create one consent that shares data.

--- c2s-ng-shared/build.sh
+++ c2s-ng-shared/build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-echo cleaning .ng_build folder
+RED='\033[0;31m'
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${LIGHT_GREEN}running npm prune and npm update${NC}"
+npm prune
+npm update
+
+echo -e "${LIGHT_GREEN}cleaning .ng_build folder${NC}"
 rm -r .ng_build/
-echo running packagr
+
+echo -e "${LIGHT_GREEN}running pre_build_copy_flex_layout script to copy flex dependencies${NC}"
+./pre_build_copy_flex_layout.sh
+
+echo -e "${LIGHT_GREEN}running packagr${NC}"
 npm run packagr
+
+echo -e "${LIGHT_GREEN}build is done${NC}"
